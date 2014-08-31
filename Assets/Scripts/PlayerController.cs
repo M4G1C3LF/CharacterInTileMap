5ff987a5232f2ac7e0e4f2390bfac77de4e4f744
M4G1C3LF <sbalaguercarmona@gmail.com>
2014-08-31 23:01:12 +0200

    Creado "Mechero"
    Animacion de luz.
    
    Presionar botón Fire1 para encender el mechero y soltar botón para apagarlo.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	GameObject link;
+	Animator linkAnimator;
+
+	GameObject darkness;
+	Animator darknessAnimator;
+
+	float verticalAxis; 
+	float horizontalAxis;
+	Vector3 localScale;
+
+
+	int moveSideState;
+
+
+	public float translationSpeed = 0.01f;	//USE THIS VAR TO CONFIG THE SPEED OF THE PLAYER
+
+
+
+
+	public void checkAnimation()
+	{
+
+		linkAnimator.SetFloat ("verticalAxis",verticalAxis);
+
+		if (verticalAxis != 0)
+		{
+			linkAnimator.SetBool("isMoving",true);
+
+			link.transform.localScale = localScale;
+		
+		}
+
+
+		if (horizontalAxis != 0)
+		{
+			linkAnimator.SetBool ("isMoving",true);
+			
+			
+			if(horizontalAxis > 0)
+			{
+				
+				linkAnimator.SetBool ("isMoving",true);
+				linkAnimator.SetBool ("movingAtSide",true);
+				
+				
+				link.transform.localScale = localScale;
+				
+			}
+			else if (horizontalAxis < 0)
+			{
+				linkAnimator.SetBool ("movingAtSide",true);
+
+				
+				if (linkAnimator.GetCurrentAnimatorStateInfo(0).nameHash == moveSideState)
+				{
+					link.transform.localScale = new Vector3 (-localScale.x,localScale.y,localScale.z);
+				}
+				
+			}
+		}
+		else
+		{
+			
+			linkAnimator.SetBool ("movingAtSide",false);
+		}
+
+		
+		if (horizontalAxis == 0 && verticalAxis == 0)
+		{
+			linkAnimator.SetBool ("isMoving",false);
+
+		}
+
+	}
+	public void getAxis()
+	{
+		verticalAxis = Input.GetAxis ("Vertical");
+		horizontalAxis = Input.GetAxis ("Horizontal");
+	}
+
+
+	public void checkMovement()
+	{
+		float x = horizontalAxis*translationSpeed;
+		float y = verticalAxis*translationSpeed;
+
+		this.transform.position = new Vector3 (this.transform.position.x + x, this.transform.position.y + y, this.transform.position.z);
+	}
+
+	public void checkLighter()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			darknessAnimator.SetBool ("lightEnabled",true);
+		}
+		else
+		{
+			darknessAnimator.SetBool ("lightEnabled",false);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+		link = GameObject.Find ("LinkSprite");
+		linkAnimator = link.GetComponent<Animator> ();
+		localScale =link.transform.localScale;
+		moveSideState = Animator.StringToHash("Moves.MoveSide");
+
+		darkness = GameObject.Find ("Darkness");
+		darknessAnimator = darkness.GetComponent<Animator> ();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		getAxis ();
+		checkAnimation ();
+		checkMovement ();
+		checkLighter();
+
+	}
+}
