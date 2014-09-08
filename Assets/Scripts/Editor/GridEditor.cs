5f42fc5666370e622e5fa24aab24edd89582bf99
M4G1C3LF <sbalaguercarmona@gmail.com>
2014-09-08 22:54:57 +0200

    GridEditor
    Se puede pintar prefabs con el ratón.
    Hay que activar la casilla Drawing del menú.

--- Assets/Scripts/Editor/GridEditor.cs
+++ Assets/Scripts/Editor/GridEditor.cs
@@ -2,12 +2,20 @@
 using UnityEditor;
 using System.Collections;
 
+enum DRAWOPTION {select, paint, paintover, erase};
+
+
 [CustomEditor (typeof(Grid))]
 public class GridEditor : Editor
 {
+
 	Grid grid;
 	private static GameObject parentObj;
 	private static GameObject prefabObj;
+
+
+	private static bool isDraw;
+	private static DRAWOPTION selected;
 
 	public void OnEnable()
 	{
@@ -18,30 +26,29 @@ public class GridEditor : Editor
 	public void loadGUIParameters()
 	{
 		GUILayout.BeginHorizontal();
-		GUILayout.Label(" Grid Width ");
-		
-		grid.width = EditorGUILayout.FloatField(grid.width, GUILayout.Width(50));
-		
+			GUILayout.Label(" Grid Width ");
+			grid.width = EditorGUILayout.FloatField(grid.width, GUILayout.Width(50));
 		GUILayout.EndHorizontal();
 		
 		GUILayout.BeginHorizontal();
-		GUILayout.Label(" Grid Height ");
-		grid.height = EditorGUILayout.FloatField(grid.height, GUILayout.Width(50));
+			GUILayout.Label(" Grid Height ");
+			grid.height = EditorGUILayout.FloatField(grid.height, GUILayout.Width(50));
 		GUILayout.EndHorizontal();
 
 		GUILayout.BeginHorizontal();
-		EditorGUILayout.LabelField("Parent Object", GUILayout.Width(256));
-
-		parentObj = (GameObject)EditorGUILayout.ObjectField(parentObj, typeof(GameObject),true,GUILayout.Width(100));
+			EditorGUILayout.LabelField("Parent Object");
+			parentObj = (GameObject)EditorGUILayout.ObjectField(parentObj, typeof(GameObject),true,GUILayout.Width(150));
 		GUILayout.EndHorizontal();
 
 		GUILayout.BeginHorizontal();
-		EditorGUILayout.LabelField("Prefab", GUILayout.Width(256));
-		
-		prefabObj = (GameObject)EditorGUILayout.ObjectField(prefabObj, typeof(GameObject),true,GUILayout.Width(100));
-
-
+			EditorGUILayout.LabelField("Prefab");
+			prefabObj = (GameObject)EditorGUILayout.ObjectField(prefabObj, typeof(GameObject),true,GUILayout.Width(150));
 		GUILayout.EndHorizontal();
+
+		GUILayout.BeginHorizontal ();
+			EditorGUILayout.LabelField ("Drawing");
+			isDraw = EditorGUILayout.Toggle(isDraw, GUILayout.Width(16));
+		GUILayout.EndHorizontal ();
 
 	}
 
@@ -76,26 +83,30 @@ public class GridEditor : Editor
 
 		Ray r = Camera.current.ScreenPointToRay(new Vector3(e.mousePosition.x, -e.mousePosition.y + Camera.current.pixelHeight));
 		Vector3 mousePos = r.origin;
-		
-		if (e.isKey && e.character == 'a')	//Clona el objeto seleccionado en la posicion del raton, centrado en el grid.
+
+		if (isDraw)
 		{
-			GameObject obj;
-			//Object prefab = EditorUtility.GetPrefabParent(Selection.activeObject);
-
-			Object prefab = prefabObj;
-			
-			if (prefab)
+			//if (e.isKey && e.character == 'a')	//Clona el objeto seleccionado en la posicion del raton, centrado en el grid.
+			if ((e.type == EventType.MouseDrag || e.type == EventType.MouseDown) && e.button == 0)
 			{
-				//Undo.IncrementCurrentEventIndex(); //NO RECONOCE ESTE METODO
-				obj = (GameObject)EditorUtility.InstantiatePrefab(prefab);
-				Vector3 aligned = new Vector3(Mathf.Floor(mousePos.x/grid.width)*grid.width + grid.width/2.0f,
-				                              Mathf.Floor(mousePos.y/grid.height)*grid.height + grid.height/2.0f, 0.0f);
-				obj.transform.position = aligned;
-				Undo.RegisterCreatedObjectUndo(obj, "Create " + obj.name);
+				GameObject obj;
+				//Object prefab = EditorUtility.GetPrefabParent(Selection.activeObject);
 
-				if (parentObj != null)
+				Object prefab = prefabObj;
+				
+				if (prefab)
 				{
-					obj.transform.parent = parentObj.transform;
+					//Undo.IncrementCurrentEventIndex(); //NO RECONOCE ESTE METODO
+					obj = (GameObject)EditorUtility.InstantiatePrefab(prefab);
+					Vector3 aligned = new Vector3(Mathf.Floor(mousePos.x/grid.width)*grid.width + grid.width/2.0f,
+					                              Mathf.Floor(mousePos.y/grid.height)*grid.height + grid.height/2.0f, 0.0f);
+					obj.transform.position = aligned;
+					Undo.RegisterCreatedObjectUndo(obj, "Create " + obj.name);
+
+					if (parentObj != null)
+					{
+						obj.transform.parent = parentObj.transform;
+					}
 				}
 			}
 		}
