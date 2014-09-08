d51647c3c5236351b6f36cf043f294c8781c9c56
M4G1C3LF <sbalaguercarmona@gmail.com>
2014-09-09 01:05:24 +0200

    Añadido holeTile.

---
+++ Assets/Scripts/HoleTile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleTile : Tile {
+
+	void OnTriggerStay2D(Collider2D collider)
+	{
+		if (collider.tag == "Player")
+		{
+			collider.GetComponentInChildren<Animator>().SetBool ("dead",true);
+		}
+		
+	}
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update ();
+		
+
+	}
+}
